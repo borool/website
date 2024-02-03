50be35131f7c6fcde38e2eb98b595b6593f60591
borool <o.borozenets3@gmail.com>
2024-02-03 22:46:06 +0200

    removed blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,13 +46,12 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
+        blog: false,
+        // {
+        //  showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        // },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -76,11 +75,10 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'My work',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/borool/website',
             label: 'GitHub',
             position: 'right',
           },
