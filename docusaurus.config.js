63f69a5e1c71ef93149c6a23207aa8f29c1e1a37
borool <o.borozenets3@gmail.com>
2024-02-03 22:52:01 +0200

    renamed site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'My site',
+  tagline: 'My documentation site',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -65,9 +65,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'My site',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'My site Logo',
           src: 'img/logo.svg',
         },
         items: [
