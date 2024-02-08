c5f209ca81fc598b6d9f46d8d2ceecbfb24c475c
borool <o.borozenets3@gmail.com>
2024-02-09 01:07:49 +0200

    added info about me, fixed homepage

--- docs/about.md
+++ docs/about.md
@@ -3,3 +3,5 @@ sidebar_position: 1
 ---
 
 # About me
+
+I am interested in technical and UX-writing. I enjoy editing text, learning new technologies, and writing trends. At the moment I am learning the “docs as code” approach. In my work I try to produce quality content within deadlines.
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,43 +3,19 @@ import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 const FeatureList = [
-/*
   {
     title: '',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
+        This is my first Docusaurus site. Here I show examples of my commercial work and examples of my writing. I will try to add more content in the future.
       </>
     ),
   },
-*/
-  {
-    title: '',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        This site includes examples of my commercial work and examples of my writing
-      </>
-    ),
-  }
-/*
-  {
-    title: '',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-      </>
-    ),
-  },
-*/
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({title, description}) {
   return (
-    <div className={clsx('col col--1')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
+    <div className={clsx('col col--12')}>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
