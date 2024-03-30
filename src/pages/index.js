74c4d2cb0039120a90ff190b926836db8d190fc7
borool <o.borozenets3@gmail.com>
2024-03-30 10:32:02 +0200

    fixed links

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/commercial">
-            My work
+            to="/docs/about">
+            About me
           </Link>
         </div>
       </div>
